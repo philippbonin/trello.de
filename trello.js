59a19a9851bdcd7761252e7676376f692a9c9fab
Philipp Bonin <p.bonin@telekom.de>
2017-05-14 09:34:27 +0200

    refactor of cookieconsent incl. source into project

--- trello.js
+++ trello.js
@@ -1,68 +1,88 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     enableGA();
     var instance = new window.cookieconsent.Popup({
-    palette: {
-    "popup": {
-    "background": "#eaf7f7",
-    "text": "#5c7291"
-    },
-"button": {
-    "background": "#56cbdb",
-    "text": "#ffffff"
-    }
-},
-type: "opt-out",
-content: {
-    "dismiss": "Accept",
-    "deny": "Decline",
-    "href": "http://trello.de/dataprivacy.html"
-    },
-cookie:{
-  "name":"trelloDeCookie",
-  "path":"/",
-  "domain":"trello.de"
-},
-    animateRevokable:false,
-onStatusChange: function(status, chosenBefore) {
-    var type = this.options.type;
-    var didConsent = this.hasConsented();
-    if (didConsent){
-    enableGA()
-    }
-if (type == 'opt-out' && !didConsent) {
-    delete_cookie("_ga");
-    delete_cookie("_gat");
-    delete_cookie("_gid");
-    delete_cookie("trelloDeCookie");
-}
-}
-});
-if (!instance.hasAnswered()){
-    instance.open();
-    }else if(!instance.hasConsented()){
+        palette: {
+            "popup": {
+                "background": "#eaf7f7",
+                "text": "#5c7291"
+            },
+            "button": {
+                "background": "#56cbdb",
+                "text": "#ffffff"
+            }
+        },
+        type: "opt-out",
+        revokable: true,
+        dismissOnScroll: true,
+        preventOpen: false,
+        content: {
+            "allow": "Accept",
+            "deny": "Decline",
+            "href": "http://trello.de/dataprivacy.html"
+        },
+        cookie: {
+            "name": "trelloDeCookie",
+            "path": "/",
+            "domain": "trello.de"
+        },
+        animateRevokable: false,
+        onRevokeChoice: function () {
+            instance.open();
+        },
+        onStatusChange: function (status, chosenBefore) {
+            var type = this.options.type;
+            var didConsent = this.hasConsented();
+            if (didConsent) {
+                enableGA()
+            }
+            if (type == 'opt-out' && !didConsent) {
+                delete_cookie("_ga");
+                delete_cookie("_gat");
+                delete_cookie("_gid");
+                delete_cookie("trelloDeCookie");
+            }
+        }
+    });
+
     instance.toggleRevokeButton(true);
+    if (!instance.hasAnswered()) {
+        instance.open();
+    } else if (instance.hasConsented() && instance.isOpen()) {
+        instance.close();
     }
-document.body.appendChild(instance.element);
+
+    document.body.appendChild(instance.element);
 });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 
-var trackOutboundLink = function(url) {
+var trackOutboundLink = function (url) {
     ga('send', 'event', 'outbound to trello.com', 'click on link', url, {
         'transport': 'beacon',
-        'hitCallback': function(){document.location = url;}
-});
+        'hitCallback': function () {
+            document.location = url;
+        }
+    });
 }
 
-var trackOutboundImage = function(url) {
+var trackOutboundImage = function (url) {
     ga('send', 'event', 'outbound to trello.com', 'click on Img', url, {
         'transport': 'beacon',
-        'hitCallback': function(){document.location = url;}
-});
+        'hitCallback': function () {
+            document.location = url;
+        }
+    });
 }
 
 function handleStayOnPage(event) {
@@ -74,27 +94,29 @@ function handleStayOnPage(event) {
     });
 }
 
-var enableGA = function(){
+var enableGA = function () {
     ga('create', 'UA-90457568-1', 'auto');
     ga('set', 'anonymizeIp', true);
     ga('send', 'pageview');
-    }
+}
 
-var delete_cookie = function(name) {
+var delete_cookie = function (name) {
     var domain = domain || document.domain;
     var path = path || "/";
-        document.cookie = name +"=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=" + domain + "; path=" + path;
-    };
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=" + domain + "; path=" + path;
+};
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#content-wrapper').hide();
 
     $('body').addClass('stop-scrolling');
-    setTimeout(function(){
-    document.getElementById("myNav").style.height = "100%";
-    $('body').bind('touchmove', function(e){e.preventDefault()});
-}, 1000);
+    setTimeout(function () {
+        document.getElementById("myNav").style.height = "100%";
+        $('body').bind('touchmove', function (e) {
+            e.preventDefault()
+        });
+    }, 1000);
 });
 
 function closeOverlay() {
@@ -103,4 +125,4 @@ function closeOverlay() {
     $('body').unbind('touchmove');
     $('body').removeClass('stop-scrolling');
     $('#content-wrapper').show();
-    }
+}
